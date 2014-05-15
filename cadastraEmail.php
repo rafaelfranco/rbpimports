5470ebecefcdd30adfd48d5ebd277c7fae4d681f
Rafael Franco <rafaelfranco@me.com>
2014-05-15 12:06:24 -0300

    Envia email inicial do avise-me

--- cadastraEmail.php
+++ cadastraEmail.php
@@ -4,11 +4,33 @@
 	mysql_connect(DB_HOSTNAME,DB_USERNAME,DB_PASSWORD);
 	mysql_select_db(DB_DATABASE);
 
-	$email = $_GET['email'];
-	$produto = $_GET['produto'];
+	$email = $_REQUEST['email'];
+	$produto = $_REQUEST['produto'];
 
 	$sql = "INSERT INTO `oc_notifyme` (`id`, `email`, `product`, `notified`) 
 			VALUES(null, '$email', $produto, 0);";
 	mysql_query($sql);
+
+	//envia email de notificação
+	$html = 'Prezado Cliente,
+
+Muito obrigado por visitar e comprar em nosso site. É um prazer tê-lo como cliente!
+
+Você se cadastrou no sistema “avise-me quando disponível”.
+
+Infelizmente no momento não temos em estoque o produto que deseja, mas assim que tiver disponível em estoque entraremos em contato pelo email cadastrado.
+ 
+Atenciosamente,
+RBP Eletrônicos
+www.rbpimports.com
+';
+
+	#envio a notificação para o lojista
+	$headers  = 'MIME-Version: 1.0' . "\r\n";
+    $headers .= 'Content-type: text/html; charset=UTF-8' . "\r\n";
+    $headers .= 'From: Pedidos Fashionera <alerta@rbpimports.com.br>' . "\r\n";;
+   
+    mail($email, '[RBP Imports] Avise-me quando disponivel', $html ,$headers);
+
 	
 ?>
