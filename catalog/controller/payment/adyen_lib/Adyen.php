47d56005715aa759415993a51f7224a05ea91a01
Rafael Franco <rafaelfranco@me.com>
2014-12-04 15:22:33 -0200

    http://www.rbpimports.com.br/loja/Payment.wsdl

--- catalog/controller/payment/adyen_lib/Adyen.php
+++ catalog/controller/payment/adyen_lib/Adyen.php
@@ -9,7 +9,7 @@ class Adyen {
 
 		//Modificação realizada via N49 05/12/2013
 		#$this->client = new SoapClient( "https://pal-$host.adyen.com/pal/Payment.wsdl",
-		$this->client = new SoapClient( "http://www.rbpimports.com.br/loja/Payment.wsdl",
+		$this->client = new SoapClient( "http://www.rbpimports.com.br/Payment.wsdl",
 		  array(
 		    "location" => "https://pal-$host.adyen.com/pal/servlet/soap/Payment",
 		    "login" => $login,
