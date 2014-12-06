0611811cac8488981d46df6e1a0c05d68f888c2d
Rafael Franco <rafaelfranco@me.com>
2014-12-06 19:11:47 -0200

    cep

--- catalog/view/theme/theme259/js/rbpimports.js
+++ catalog/view/theme/theme259/js/rbpimports.js
@@ -45,6 +45,19 @@ jQuery(document).ready(function($) {
 
 
 });
+
+function onlyNumbers(evt)
+{
+    var e = event || evt; // for trans-browser compatibility
+    var charCode = e.which || e.keyCode;
+    
+    if (charCode > 31 && (charCode < 48 || charCode > 57))
+        return false;
+
+    return true;
+
+}
+
 function preencheEndereco(cependereco) {
     $.ajax({
             url: '/index.php?route=module/cep',
